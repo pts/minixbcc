3401fab7f235461b8ee4ff78ea42e7f812851a16
Peter Szabo <pts@fazekas.hu>
2026-01-08 23:39:32 +0100

    added compile.sh

---
+++ cpp/compile.sh
@@ -0,0 +1,9 @@
+#! /bin/sh --
+# by pts@fazekas.hu at Thu Jan  8 23:03:03 CET 2026
+set -ex
+test "$0" = "${0%/*}" || cd "${0%/*}"
+
+gcc -m32 -s -O2 -DOLD_PREPROCESSOR -DFILE_LOCAL=static -Dunix -o cpp cpp1.c cpp2.c cpp3.c cpp4.c cpp5.c cpp6.c
+ls -l cpp
+
+: "$0" OK.
