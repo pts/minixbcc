96ae564b9488c9bbec28a6a81d6ddb3c5185b1e9
Peter Szabo <pts@fazekas.hu>
2026-01-20 22:35:58 +0100

    made libc/isatty.c work on Minix >=1.7.4 as well

--- libc/isatty.c
+++ libc/isatty.c
@@ -5,8 +5,15 @@
 int isatty(fd)
 int fd;
 {
-  _M.TTY_REQUEST = TIOCGETP;
+  char dummy[sizeof(int) == 2 ? 32 : 36];  /* struct termios dummy; */  /* For compatibility with Minix 1.7.4--2.0.4--3.2.0. */
+
+  _M.TTY_REQUEST = 0x7408;  /* TIOCGETP; */
   _M.TTY_LINE = fd;
-  if (callx(FS, IOCTL) < 0) return(0);
+  if (callx(FS, IOCTL) >= 0) goto found_tty;  /* Minix 1.5--1.7.2. */
+  _M.TTY_REQUEST = (unsigned) (0x80245408L & ~(unsigned) 0);  /* TCGETS. */
+  _M.TTY_LINE = fd;
+  _M.ADDRESS = dummy;
+  if (callx(FS, IOCTL) < 0) return 0;  /* Minix 1.7.4--2.0.4--3.2.0. */
+ found_tty:
   return(1);
 }
