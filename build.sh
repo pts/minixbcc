679a9e990c3577b57bb4fcf27dfc5c388b1c8786
Peter Szabo <pts@fazekas.hu>
2026-01-26 06:12:16 +0100

    refactor: moved building the libc from build.sh to cross.sh

--- build.sh
+++ build.sh
@@ -141,6 +141,7 @@ if test "$1" = gcc || test "$1" = clang || test "$1" = owcc || test "$1" = minic
     "$cc" $cc2 $cflags $@ -o libexec/cpp cpp/cpp1.c cpp/cpp2.c cpp/cpp3.c cpp/cpp4.c cpp/cpp5.c cpp/cpp6.c || exit "$?"
   ) || exit "$?"
   h="`cat h.out`"
+  rm -f h.out
   test "$h" || exit "$?"
   if test "$h" != 0; then
     set cross  # For subsequent `test "$1" = ...'.
@@ -398,70 +399,15 @@ mv cr.tool 3/cr || exit "$?"
 
 # --- Build the target libc object files for each system.
 #
-# We don't need the target tools for this, because we use the host tools ("$sc", "$as", "$ld").
+# We don't need the target tools for this, because we use the host tools ("$sc", "$as").
 
-# Relevant flags of the assembler ($as):
-# -0  start with 16-bit code segment; default: native
-# -3  start with 32-bit code segment; default: native
-# -a  enable partial source compatibility with Minix 1.5.10 asld
-# -g  only put global symbols in object file  # !! Start using it.
-# -n name.s  module name (without extension) to put to .o file
-# -o name.o  produce object file
-# -u  take undefined symbols as imported-with-unspecified segment
-# -w  don't print warnings
-
-ANSIB='abort abs assert atoi atol bsearch ctime ctype errno exit fclose fflush fgetc fgets fopen fprintf fputc fputs fread freopen fseek ftell fwrite getenv gets malloc perror puts qsort rand scanf setbuf signal sprintf strerror strtol strtoul system time tmpnam ungetc vsprintf'  # C source files (*.c) from Minix 1.5.10 /usr/src/lib/ansi . Patched by Bruce Evans.
-IBMB='peekpoke portio'  # C source files (*.c) from Minix 1.5.10 /usr/src/lib/ibm . Patched by Bruce Evans.
-OTHERB='amoeba bcmp bcopy brk bzero call chroot cleanup crypt curses doprintf ffs getdents getopt getpass gtty index ioctl itoa lock lrand lsearch memccpy message mknod mktemp mount nlist popen printdat printk prints ptrace putenv regexp regsub rindex seekdir stb stderr stime stty swab sync syslib telldir termcap umount uniqport vectab'  # C source files (*.c) from Minix 1.5.10 /usr/src/lib/other . Patched by Bruce Evans.
-POSIXB='_exit access alarm chdir chmod chown close closedir creat ctermid cuserid dup dup2 exec execlp fcntl fdopen fork fpathconf fstat getcwd getegid geteuid getgid getgrent getlogin getpid getppid getpwent getuid isatty kill link lseek mkdir mkfifo open opendir pathconf pause pipe read readdir rename rewinddir rmdir setgid setuid sleep stat sysconf times ttyname umask unlink utime wait write'  # C source files (*.c) from Minix 1.5.10 /usr/src/lib/posix . Patched by Bruce Evans.
-SB='crtso head'  # Minix startup routines, assembly source files (*.s) by Bruce Evans. !! Should head.s be removed from libc? A file named like this seems to be used for the Minix kernel, but doesn't it have its own copy?
-IB='idiv idivu imod imodu imul isl isr isru'  # BCC compiler support for integer arithmetic, assembly source files (*.s) by Bruce Evans.
-MB='brksize catchsig sendrec setjmp'  # Minix library routines, assembly source files (*.s) by Bruce Evans.
-STRMASMB='memcmp memmove memset strcmp strcpy strlen strncmp strncpy'  # Minix library string routines, assembly source files (*.s) with i86 code based on to-be-preprecessed assembly source files (*.x) from Minix 1.5.10 /usr/src/lib/string, and i386 code copied from inline assembly in C source files (*.c) by Bruce Evans.
-
-for a03 in 0 3; do
-  if test "$a03" = 0; then  # i86.
-    LLB='laddl landl lcmpl lcoml ldecl ldivl ldivul leorl lincl lmodl lmodul lmull lnegl lorl lsll lsrl lsrul lsubl ltstl ldivmod'  # BCC compiler support for long arithmetic on little-endian (x86) longs, assembly source files (*.s) by Bruce Evans.
-    STRASMB='memchr strcat strncat strchr strcoll strrchr strspn strstr strtok strxfrm strpbrk strcspn'  # Minix library string routines, assembly source files (*.s) created manually from to-be-preprecessed assembly source files (*.x) from Minix 1.5.10 /usr/src/lib/string . Patched by Bruce Evans.
-    STRCCCB=''  # No additional string routines, C source files (*.c). These are implemented in STRASMB instead.
-  else  # i386.
-    LLB=''  # No additional BCC compiler support for long arithmetic on little-endian (x86) longs, assembly source files (*.s).
-    STRASMB=''  # No additional Minix library string routines. These are implemented in STRCCCB instead.
-    STRCCCB='memchr strcat strncat strchr strcoll strrchr strspn strstr strtok strxfrm strpbrk strcspn'  # Minix library string routines, C source files (*.c) without assembly from Minix 1.5.10 /usr/src/lib/string . Patched by Bruce Evans.
-  fi
-
-  test -d "$a03" || mkdir "$a03" || exit "$?"
-
-  for b in $STRMASMB $STRASMB $SB $IB $JB $LBB $LLB $MB; do
-    "$as" -"$a03" -w $aflag -o "$a03"/"$b".o libc/"$b".s || exit "$?"
-    "$cmp" "$a03"/"$b".o "$a03"g/"$b".n || exit "$?"  # Compare object file (*.o) to golden file (*.n).
-  done
-
-  for b in $STRCCCB $ANSIB $IBMB $OTHERB $POSIXB; do
-    # bcc -v -"$a03" -c -O -D_MINIX -D_POSIX_SOURCE -o "$a03"/"$b".o "$b".c
-    "$sc" -"$a03" -Iinclude -D_MINIX -D_POSIX_SOURCE -o "$a03"/"$b".s libc/"$b".c || exit "$?"
-    "$as" -"$a03" -u -w -o "$a03"/"$b".o "$a03"/"$b".s || exit "$?"
-    "$cmp" "$a03"/"$b".o "$a03"g/"$b".n || exit "$?"  # Compare object file (*.o) to the relavant v3 golden file (*.m).
-    rm -f "$a03"/"$b".s || exit "$?"
-  done
-
-  # ($earlstdiodir) Not compiling estdio.
-  # ($localdir) No directory /usr/src/lib/local .
-done
+sh cross.sh --libc "$sc" "$as" "$cr" "$cmp" || exit "$?"
 
 # --- Build the target libc library archive (*.a) files for each system.
 #
 # We don't need the target tools for this, because we use the host tools ("$cr").
 
-cru="$cr"; case "$cr" in /*) ;; */*) cru="../$cr" ;; esac  # "$cru" is "$cr", but it works from one directory lower.
-# !! Increment typestamp because of changes in isatty.c etc.
-# !! Redo both in alphabetic order.
-(cd 0 && "$cru" -o libc.a -t @644973285 _exit.o abort.o abs.o access.o alarm.o amoeba.o assert.o atoi.o atol.o bcmp.o bcopy.o brk.o brksize.o bsearch.o bzero.o call.o catchsig.o chdir.o chmod.o chown.o chroot.o cleanup.o close.o closedir.o creat.o crypt.o ctermid.o ctime.o ctype.o curses.o cuserid.o doprintf.o dup.o dup2.o errno.o exec.o execlp.o exit.o fclose.o fcntl.o fdopen.o fflush.o ffs.o fgetc.o fgets.o fopen.o fork.o fpathconf.o fprintf.o fputc.o fputs.o fread.o freopen.o fseek.o fstat.o ftell.o fwrite.o getcwd.o getdents.o getegid.o getenv.o geteuid.o getgid.o getgrent.o getlogin.o getopt.o getpass.o getpid.o getppid.o getpwent.o gets.o getuid.o gtty.o idiv.o idivu.o imod.o imodu.o imul.o index.o ioctl.o isatty.o isl.o isr.o isru.o itoa.o kill.o laddl.o landl.o lcmpl.o lcoml.o ldecl.o ldivl.o ldivmod.o ldivul.o leorl.o lincl.o link.o lmodl.o lmodul.o lmull.o lnegl.o lock.o lorl.o lrand.o lsearch.o lseek.o lsll.o lsrl.o lsrul.o lsubl.o ltstl.o malloc.o memccpy.o memchr.o memcmp.o memmove.o memset.o message.o mkdir.o mkfifo.o mknod.o mktemp.o mount.o nlist.o open.o opendir.o pathconf.o pause.o peekpoke.o perror.o pipe.o popen.o portio.o printdat.o printk.o prints.o ptrace.o putenv.o puts.o qsort.o rand.o read.o readdir.o regexp.o regsub.o rename.o rewinddir.o rindex.o rmdir.o scanf.o seekdir.o sendrec.o setbuf.o setgid.o setjmp.o setuid.o signal.o sleep.o sprintf.o stat.o stb.o stderr.o stime.o strcat.o strchr.o strcmp.o strcoll.o strcpy.o strcspn.o strerror.o strlen.o strncat.o strncmp.o strncpy.o strpbrk.o strrchr.o strspn.o strstr.o strtok.o strtol.o strtoul.o strxfrm.o stty.o swab.o sync.o sysconf.o syslib.o system.o telldir.o termcap.o time.o times.o tmpnam.o ttyname.o umask.o umount.o ungetc.o uniqport.o unlink.o utime.o vectab.o vsprintf.o wait.o write.o) || exit "$?"
-"$cmp" 0/libc.a 0g/libc.d || exit "$?"
-#cmp -l 0/libc.w 0/libc.a ||:  # Mismatch in the even padding bytes.
-(cd 3 && "$cru" -o libc.a -t @645451200 abort.o abs.o assert.o atoi.o atol.o bsearch.o ctime.o ctype.o errno.o exit.o fclose.o fflush.o fgetc.o fgets.o fopen.o fprintf.o fputc.o fputs.o fread.o freopen.o fseek.o ftell.o fwrite.o getenv.o gets.o malloc.o memchr.o memmove.o perror.o puts.o qsort.o rand.o scanf.o setbuf.o signal.o sprintf.o strcat.o strchr.o strcoll.o strerror.o strrchr.o strspn.o strstr.o strtok.o strtol.o strtoul.o strxfrm.o system.o time.o tmpnam.o ungetc.o vsprintf.o peekpoke.o portio.o amoeba.o bcmp.o bcopy.o brk.o bzero.o call.o chroot.o cleanup.o crypt.o curses.o doprintf.o ffs.o getdents.o getopt.o getpass.o gtty.o index.o ioctl.o itoa.o lock.o lrand.o lsearch.o memccpy.o message.o mknod.o mktemp.o mount.o nlist.o popen.o printdat.o printk.o prints.o ptrace.o putenv.o regexp.o regsub.o rindex.o seekdir.o stb.o stderr.o stime.o stty.o swab.o sync.o syslib.o telldir.o termcap.o umount.o uniqport.o vectab.o _exit.o access.o alarm.o chdir.o chmod.o chown.o close.o closedir.o creat.o ctermid.o cuserid.o dup.o dup2.o exec.o execlp.o fcntl.o fdopen.o fork.o fpathconf.o fstat.o getcwd.o getegid.o geteuid.o getgid.o getgrent.o getlogin.o getpid.o getppid.o getpwent.o getuid.o isatty.o kill.o link.o lseek.o mkdir.o mkfifo.o open.o opendir.o pathconf.o pause.o pipe.o read.o readdir.o rename.o rewinddir.o rmdir.o setgid.o setuid.o sleep.o stat.o sysconf.o times.o ttyname.o umask.o unlink.o utime.o wait.o write.o memcmp.o memset.o strcmp.o strcpy.o strcspn.o strlen.o strncat.o strncmp.o strncpy.o strpbrk.o idiv.o idivu.o imod.o imodu.o imul.o isl.o isr.o isru.o brksize.o catchsig.o sendrec.o setjmp.o) || exit "$?"
-"$cmp" 3/libc.a 3g/libc.d || exit "$?"
-#cmp -l 3/libc.w 3/libc.a ||:  # Mismatch in $uid, $gid, $mtime and in the even padding bytes.
+:  # cross.sh above has done it already.
 
 # --- Build the extra tools for each system.
 #
